f469bc9718307bf6674a81bf17d7a2aeaee5f2d3
williamsza <wyllsza@gmail.com>
2025-07-13 12:21:11 -0300

    Fist commit

---
+++ src/test/java/com/exceptions/exercicios/ExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exceptions.exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
